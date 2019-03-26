e1bf77db61201c342355366f69a890d74090c3ab
Jessica Lin <jessicaa.lin@gmail.com>
2019-03-26 17:13:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine (katzDeliLine){
     var katzDeliLineDetail = [];
     for (var i=0; i<katzDeliLine.length; i++){
       katzDeliLineDetail.push(`${i+1}. ${katzDeliLine[i]}`);
-    }return 'The line is currently: '+katzDeliLineDetail.join(',');
+    }return 'The line is currently: '+katzDeliLineDetail.join(', ');
     /*var string = '';
     var i = 0;
     do {
